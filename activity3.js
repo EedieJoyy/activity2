78b4d77e06a681d2536e9904f11a76a0ec5fbf2f
EedieJoyy <111791658+EedieJoyy@users.noreply.github.com>
2022-10-23 20:36:17 +0800

    Delete activity3.js

--- activity3.js
+++
@@ -1,4 +0,0 @@
-function alph_order(str) {
-    return str.split("").sort().join("")
-}console.log(alph_order("eddiejoydampiganon"))
- console.log(alph_order("bachelor of science in information technology"))
